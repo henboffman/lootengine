d13d1c809a63d7fd61836f8959db153cd00cf11e
henboffman <benbrent@gmail.com>
2017-06-19 21:25:09 -0400

    Added bid logic

--- lootengine/Models/Auction.cs
+++ lootengine/Models/Auction.cs
@@ -11,9 +11,48 @@ namespace lootengine.Models
         public int Id{ get; set; }
         public int ItemId{ get; set; }
         public DateTime Created{ get; set; }
-        public string CreatedBy{ get; set; }
+        public int CreatedBy{ get; set; }
+        public DateTime Expires { get; set; }
+        public int LastBidder { get; set; }
+        public int Duration { get; set; }
         public int CurrentBid { get; set; }
         public int Buyout { get; set; }
+
+        public Auction ()
+        {
+
+        }
+
+        public void NewBid(int bidder, int bidAmount)
+        {
+            if (DateTime.Now < Expires)
+            {
+                if (bidder != CreatedBy)
+                {
+                    if (bidder != LastBidder)
+                    {
+                        if (bidAmount > CurrentBid)
+                        {
+                            CurrentBid = bidAmount;
+                            LastBidder = bidder;
+                        } else {
+                            throw new Exception("Bid must be greater than current bid!");
+                        }   
+                    } else
+                    {
+                        throw new Exception("You already are the last bidder!");
+                    }
+                } else
+                {
+                    throw new Exception("Can't bid on your own auction");
+                }
+            } else
+            {
+                throw new Exception("Auction has already expired!");
+            }
+
+            
+        }
 
     }
 }
