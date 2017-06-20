548dcb4432decc4188d194ed48cd03bbdab98c99
henboffman <benbrent@gmail.com>
2017-06-19 21:31:35 -0400

    refactored newBid to probably better practice

--- lootengine/Models/Auction.cs
+++ lootengine/Models/Auction.cs
@@ -8,50 +8,72 @@ namespace lootengine.Models
 {
     class Auction
     {
-        public int Id{ get; set; }
-        public int ItemId{ get; set; }
-        public DateTime Created{ get; set; }
-        public int CreatedBy{ get; set; }
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public DateTime Created { get; set; }
+        public int CreatedBy { get; set; }
         public DateTime Expires { get; set; }
         public int LastBidder { get; set; }
         public int Duration { get; set; }
         public int CurrentBid { get; set; }
         public int Buyout { get; set; }
 
-        public Auction ()
+        public Auction()
         {
 
         }
 
-        public void NewBid(int bidder, int bidAmount)
+        private bool AuctionIsActive()
         {
             if (DateTime.Now < Expires)
             {
-                if (bidder != CreatedBy)
-                {
-                    if (bidder != LastBidder)
-                    {
-                        if (bidAmount > CurrentBid)
-                        {
-                            CurrentBid = bidAmount;
-                            LastBidder = bidder;
-                        } else {
-                            throw new Exception("Bid must be greater than current bid!");
-                        }   
-                    } else
-                    {
-                        throw new Exception("You already are the last bidder!");
-                    }
-                } else
-                {
-                    throw new Exception("Can't bid on your own auction");
-                }
-            } else
+                return true;
+            }
+            else
             {
-                throw new Exception("Auction has already expired!");
+                throw new Exception("Auction has already expired");
+            }
+        }
+
+        private bool BidderIsValid(int bidder)
+        {
+            if (bidder != CreatedBy)
+            {
+                if (bidder != LastBidder)
+                {
+                    return true;
+                }
+                else
+                {
+                    throw new Exception("Bidder is already current high bid");
+                }
+            }
+            else
+            {
+                throw new Exception("Bidder cannot be auction creator");
+            }
+        }
+
+        public void NewBid(int bidder, int bidAmount)
+        {
+            if (AuctionIsActive()
+            {
+                if (BidderIsValid(bidder))
+                {
+
+                    if (bidAmount > CurrentBid)
+                    {
+                        CurrentBid = bidAmount;
+                        LastBidder = bidder;
+                    }
+                    else
+                    {
+                        throw new Exception("Bid must be greater than current bid!");
+                    }
+
+                } =
             }
 
-            
         }
 
     }
