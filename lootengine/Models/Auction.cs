143b0019ee56909d22fd7ce6395bb6f818c63706
henboffman <benbrent@gmail.com>
2017-06-19 21:11:27 -0400

    updated auction class

--- lootengine/Models/Auction.cs
+++ lootengine/Models/Auction.cs
@@ -8,5 +8,12 @@ namespace lootengine.Models
 {
     class Auction
     {
+        public int Id{ get; set; }
+        public int ItemId{ get; set; }
+        public DateTime Created{ get; set; }
+        public string CreatedBy{ get; set; }
+        public int CurrentBid { get; set; }
+        public int Buyout { get; set; }
+
     }
 }
