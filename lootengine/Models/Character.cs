b6ba1836b2e4c71b2dce0353c663fcaf49eccf62
henboffman <benbrent@gmail.com>
2017-06-19 20:38:34 -0400

    added level check logic for hero

--- lootengine/Models/Character.cs
+++ lootengine/Models/Character.cs
@@ -11,6 +11,7 @@ namespace lootengine.Models
         public string Name { get; set; }
         public int Level { get; set; }
         public int Experience { get; set; }
+        public int ExperienceForNextLevel { get; set; }
         public int CarryWeight { get; set; }
         public int InventoryWeight { get; set; }
         public int Damage { get; set; }
@@ -23,14 +24,44 @@ namespace lootengine.Models
             Name = "new";
             Level = 1;
             Experience = 0;
+            ExperienceForNextLevel = 400;
             CarryWeight = 20;
             InventoryWeight = 5;
             Damage = 10;
             AttackPerSecond = 1;
             Health = 250;
             Armor = 10;
+        }
 
+        /// <summary>
+        /// Determines if the hero has enough experience for next level
+        /// </summary>
+        private void CheckExperience()
+        {   
+            if (Experience >= ExperienceForNextLevel)
+            {
+                NextLevel();
+                GetExperienceForNextLevel();
+            }
+        }
 
+        /// <summary>
+        /// Increments the character level
+        /// </summary>
+        private void NextLevel()
+        {
+            Console.WriteLine("Level Increased!");            
+            Level++;
+        }
+
+        /// <summary>
+        /// Retrieves experience required for next level
+        /// </summary>
+        private void GetExperienceForNextLevel()
+        {
+            //get and set experience required for next level
+            //this is done on level up to reduce the number of database requests to once per level
+            //ExperienceForNextLevel = Experience.Where(x => x.Level == Level + 1).Experience;
         }
     }
 }
